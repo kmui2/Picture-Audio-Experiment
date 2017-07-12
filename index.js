50a704041fc04ff175d06d099ad89078c8f651e2
Kevin Mui <kmui2@wisc.edu>
2017-07-11 21:25:24 -0500

    changed to port 8000 and connected index.html with index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var app = express()
+var path = require("path");
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
