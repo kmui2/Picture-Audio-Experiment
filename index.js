4fd9a3750adacd3731262b6332e64f42d362806a
Kevin Mui <kmui2@wisc.edu>
2017-07-27 15:01:48 -0500

    logs data using node

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var app = express()
 var path = require("path");
 const PythonShell = require('python-shell');
 const fs = require('fs');
+const _ = require('lodash');
 const csvWriter = require("csv-write-stream");
 var bodyParser = require('body-parser');
 const writer = csvWriter({sendHeaders: false});
+
+let subjCode = 'null_subjCode';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,7 +26,7 @@ app.listen(app.get('port'), function () {
 
 app.post('/trials', function (req, res) {
   console.log("trials post request received");
-  let subjCode = req.body.subjCode;
+  subjCode = req.body.subjCode;
   console.log("subjCode received is " + subjCode);
   PythonShell.defaultOptions = { args: [subjCode] };
   PythonShell.run('TYP_genTrials_v2.py', function (err, results) {
@@ -32,4 +35,24 @@ app.post('/trials', function (req, res) {
     console.log(results);
     res.send({success: results[0]});
   });
+})
+
+app.post('/data', function (req, res) {
+  console.log('data post request received');
+  let data = req.body;
+  let dataString = _.map(data, (value) => {
+    return value + '';
+  }).join('\t')+'\n';
+  let path = 'data/'+subjCode+'_test.txt';
+  if (!fs.existsSync(path)) 
+    fs.openSync(path, 'a');
+  else
+    console.log("file exists");
+  fs.appendFile(path, dataString, function (err) {
+      if (err) {
+        res.send({success: false});
+        throw err;
+      }
+      res.send({success: true});
+    } )
 })
