aebcaa471c80d8368feed854075a36e31c90e8f5
Kevin Mui <kmui2@wisc.edu>
2017-07-29 14:43:13 -0500

    adding comments and cleaning up code

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-var express = require('express')
-var app = express()
-var path = require("path");
+// Dependencies
+const express = require('express');
+const path = require("path");
 const PythonShell = require('python-shell');
 const fs = require('fs');
 const _ = require('lodash');
 const csvWriter = require("csv-write-stream");
-var bodyParser = require('body-parser');
-const writer = csvWriter({sendHeaders: false});
-const csv=require('csvtojson')
+const bodyParser = require('body-parser');
+const csv = require('csvtojson');
 
-let subjCode = 'null_subjCode';
+
+let app = express();
+let writer = csvWriter({sendHeaders: false});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
 
+// For rending HTML
+app.use(express.static(__dirname + '/public'))
 app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname + '/public/index.html'));
 })
@@ -25,26 +27,26 @@ app.listen(app.get('port'), function () {
   console.log("Node app is running at http://localhost:" + app.get('port'))
 })
 
+// POST endpoint for requesting trials
 app.post('/trials', function (req, res) {
   console.log("trials post request received");
-  subjCode = req.body.subjCode;
+  let subjCode = req.body.subjCode;
   console.log("subjCode received is " + subjCode);
+
+  // Runs genTrial python script with subjCode arg
   PythonShell.defaultOptions = { args: [subjCode] };
   PythonShell.run('TYP_genTrials_v2.py', function (err, results) {
-    // res.send(trials);
     if (err) throw err;
-    console.log(results);
-    
-
-    let csvFilePath = 'trials/trials_' + subjCode + '.csv';
     let trials = [];
+
+    // Reads generated trial csv file
     csv()
-    .fromFile(csvFilePath)
+    .fromFile('trials/trials_' + subjCode + '.csv')
+    // Push all trials to array
     .on('json',(jsonObj)=>{
-      // combine csv header row and csv line to a json object
-      // jsonObj.a ==> 1 or 4
       trials.push(jsonObj);
     })
+    // Send trials array when finished
     .on('done',(error)=>{
       if (error) {
         res.send({success: false});
@@ -57,13 +59,18 @@ app.post('/trials', function (req, res) {
   });
 })
 
+// POST endpoint for receiving trial responses
 app.post('/data', function (req, res) {
   console.log('data post request received');
-  let data = req.body;
-  let dataString = _.map(data, (value) => {
+
+  // Parses the trial response data to tab separated txt line
+  let response = req.body;
+  let dataString = _.map(response, (value) => {
     return value + '';
   }).join('\t')+'\n';
-  let path = 'data/'+subjCode+'_test.txt';
+
+  // Append response string to txt file
+  let path = 'data/'+response.subjCode+'_test.txt';
   if (!fs.existsSync(path)) 
     fs.openSync(path, 'a');
   else
